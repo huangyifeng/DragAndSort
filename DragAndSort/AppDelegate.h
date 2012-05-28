601ad262a8360e00b0fc34db24cbeac7ea8f5154
huangyifeng <luckyJerry85@gmail.com>
2012-05-28 10:17:01 +0800

    Initial Commit

---
+++ DragAndSort/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DragAndSort
+//
+//  Created by Huang YiFeng on 12-5-28.
+//  Copyright (c) 2012年 Cybozu, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DragAndSort/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragAndSort
+//
+//  Created by Huang YiFeng on 12-5-28.
+//  Copyright (c) 2012年 Cybozu, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
